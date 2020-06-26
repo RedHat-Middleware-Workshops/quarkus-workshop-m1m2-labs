791f039b2df8f79a50477fabd58a45a14f8ff135
danieloh30 <doh@redhat.com>
2020-06-26 00:17:12 -0400

    update RHBO 1.3.4 and magic script for m2

---
+++ scripts/deploy-people.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERXX=$1
+
+if [ -z "$USERXX" -o "$USERXX" = "userXX" ]
+  then
+    echo "Usage: Input your username like deploy-people.sh user1"
+    exit;
+fi
+
+echo Your username is $USERXX
+echo Deploy people service........
+
+oc project $USERXX-project
+oc delete dc,deployment,bc,build,svc,route,pod,is --all
+
+echo "Waiting 30 seconds to finialize deletion of resources..."
+sleep 30
+
+mvn package -Pnative -DskipTests -f ${CHE_PROJECTS_ROOT}/quarkus-workshop-labs
+
+oc new-build quay.io/quarkus/ubi-quarkus-native-binary-s2i:19.3.1 --binary --name=people -l app=people
+oc start-build people --from-file $CHE_PROJECTS_ROOT/quarkus-workshop-labs/target/*-runner --follow
+oc new-app people -l "app.openshift.io/runtime=quarkus,app.kubernetes.io/part-of=people" && oc expose svc/people
+oc rollout status -w dc/people
